f01db013d8ce74258f6cd3b07ed6616200d1a768
itskipronoh <kipronoyegon68@gmail.com>
2025-10-28 16:29:40 +0300

    Refactor RegionsController to improve code formatting and add AddRegion method for creating new regions
    Signed-off-by: itskipronoh <kipronoyegon68@gmail.com>

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
 
 namespace NZWalks.API.Controllers
 {
@@ -21,7 +22,7 @@ namespace NZWalks.API.Controllers
 
         // GET all regions
         [HttpGet]
-        public async Task <IActionResult> GetAllRegions()
+        public async Task<IActionResult> GetAllRegions()
         {
             var regions = await dbContext.Regions.ToListAsync();
             return Ok(regions);
@@ -37,6 +38,14 @@ namespace NZWalks.API.Controllers
                 return NotFound();
 
             return Ok(region);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddRegion(Region region)
+        {
+            region.Id = Guid.NewGuid();
+            await dbContext.Regions.AddAsync(region);
+            await dbContext.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetRegionById), new { id = region.Id }, region);
         }
     }
 }
