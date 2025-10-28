02ee59e9ee04f0708a02c0dc48a7fb8d2c5c500a
itskipronoh <kipronoyegon68@gmail.com>
2025-10-28 16:04:45 +0300

    Add initial project structure with API controllers, models, and database context
    Signed-off-by: itskipronoh <kipronoyegon68@gmail.com>

---
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using NZWalks.API.Data;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly NZWalksDbContext dbContext;
+
+        public RegionsController(NZWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // GET all regions
+        [HttpGet]
+        public async Task <IActionResult> GetAllRegions()
+        {
+            var regions = await dbContext.Regions.ToListAsync();
+            return Ok(regions);
+        }
+
+        // GET region by id
+        // GET: /api/regions/{id}
+        [HttpGet("{id:guid}")]
+        public async Task<IActionResult> GetRegionById(Guid id)
+        {
+            var region = await dbContext.Regions.FirstOrDefaultAsync(r => r.Id == id);
+            if (region == null)
+                return NotFound();
+
+            return Ok(region);
+        }
+    }
+}
